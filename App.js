d574f963e9b0980576b802bc04955ed4ddc44555
tzupingchenwork <andychentzupingwork@gmail.com>
2023-10-19 13:31:29 +0800

    feat: 用 webview 把網頁鑲嵌進應用程式

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    // <SafeAreaView style={styles.container}>
+
+    // </SafeAreaView>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <WebView source={{ uri: 'http://192.168.12.249:9527/iflood/' }} style={styles.webview} />
     </View>
   );
 }
@@ -16,5 +25,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+    marginTop: 20,
   },
 });
